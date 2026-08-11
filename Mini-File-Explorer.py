c99655e6832807934867f2dedc1ca104669de7f4
vishalkalva <kalvavishal26@gmail.com>
2026-08-11 13:06:09 +0530

    Mini-File-Explorer.py

--- Mini-File-Explorer.py
+++ Mini-File-Explorer.py
@@ -1,5 +1,11 @@
-import os
+import os 
 
 class FileExplorer:
     def __init__(self):
         self.current_path = os.getcwd()
+
+explorer = FileExplorer()
+print(explorer.current_path)
+
+
+#just working on it
